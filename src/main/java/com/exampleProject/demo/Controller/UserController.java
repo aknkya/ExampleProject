14b98f1a3d476d3e9a970f612472a31cd9cbf99c
aknkya <akn_kya@hotmail.com>
2020-02-24 12:11:53 +0300

    1.3

--- src/main/java/com/exampleProject/demo/Controller/UserController.java
+++ src/main/java/com/exampleProject/demo/Controller/UserController.java
@@ -2,17 +2,17 @@ package com.exampleProject.demo.Controller;
 
 import com.exampleProject.demo.Entity.UserEntity;
 import com.exampleProject.demo.Service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("welcome")
@@ -22,6 +22,7 @@ private final UserService userService;
     public UserController(UserService userService){
         this.userService = userService;
     }
+    @RequestMapping("/register")
     @PostMapping
     public ResponseEntity<UserEntity> Save(@Valid @RequestBody UserEntity userEntity) throws URISyntaxException{
         UserEntity p = userService.save(userEntity);
@@ -29,5 +30,19 @@ private final UserService userService;
 
 }
 
+    @GetMapping({"id"})
+    public ResponseEntity<UserEntity> findById(@PathVariable int id){
+        Optional<UserEntity> userEntity=userService.findById(id);
+        return userEntity.map(response->ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+   }
 
+   @GetMapping
+    public ResponseEntity<List<UserEntity>> findall(){
+       return ResponseEntity.ok(userService.findAll());
+    }
+   @DeleteMapping
+    public void deleteById(int id){
+        userService.deletebyId(id);
+   }
 }
